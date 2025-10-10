6472eb55a8c587d73f55ccbab223f10b273e84bb
giovanabordinhonreis <giovanabordinhon@gmail.com>
2025-10-09 21:13:42 -0300

    refactor: Correções tela de login

--- core/views.py
+++ core/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import CustomAuthenticationForm
 
-def login(request):
+def login_usuario(request):
     if request.method == 'POST':
         form = CustomAuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            # Redirecionar para o dashboard apropriado (cliente ou nutri)
-            # Por enquanto, vamos criar um dashboard genérico
             return redirect('dashboard') 
     else:
         form = CustomAuthenticationForm()
